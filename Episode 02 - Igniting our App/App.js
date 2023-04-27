d02de068ebcf7f0dea843b2c3503b7a42981a31e
Abhayngu <masterdwivedi@gmail.com>
2023-04-27 15:29:24 +0530

    some changes in e02

--- Episode 02 - Igniting our App/App.js
+++ Episode 02 - Igniting our App/App.js
@@ -1,10 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 const heading = React.createElement(
     "h1",
     { className: "someclass" },
     "Hello world"
 );
-const heading2 = React.createElement("h1", {}, "another hello world");
-root2.render(heading2);
 root.render(heading);
