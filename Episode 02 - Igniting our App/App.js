7d5ffe26acc99528b5964feedcfb6a64145a9a9e
Abhayngu <masterdwivedi@gmail.com>
2023-05-03 08:58:22 +0530

    changed folder structure and added npm scripts

--- Episode 02 - Igniting our App/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading = React.createElement(
-    "h1",
-    { className: "someclass" },
-    "Hello world"
-);
-root.render(heading);
