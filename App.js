9ebcd2e0a9bdfe4755307b5bf62e6ec40686db83
Abhayngu <masterdwivedi@gmail.com>
2023-05-03 11:22:19 +0530

    wrote ques till half lecture

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading = React.createElement(
-    "h1",
-    { className: "someclass" },
-    "Hello world"
-);
-root.render(heading);
+const jsxElement = <a href="https://www.google.com">google</a>;
+root.render(jsxElement);
